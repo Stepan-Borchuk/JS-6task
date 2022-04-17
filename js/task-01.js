0a2767aee597624c3e3583ba16cee7357ae2eb55
Stepan <91005125+Stepan-Borchuk@users.noreply.github.com>
2022-04-17 15:11:45 +0300

    2

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,3 @@
 document.querySelectorAll('.item').forEach(item => {
     console.log(`Category: ${item.querySelector('h2').textContent} Elements: ${item.querySelectorAll('li').length} `)
 })
-
-
-    
