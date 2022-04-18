1b29610d84c14a290616378e658d0b203bd92666
Stepan <91005125+Stepan-Borchuk@users.noreply.github.com>
2022-04-18 09:26:49 +0300

    9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
 
+// Валідація
+form.firstElementChild.innerHTML = '<input type="email" name="email" required="required" />';
+
+form.firstElementChild.nextElementSibling.innerHTML = '<input type="password" name="password" required minlength="6" maxlength="12" pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$" title="Please include at least 1 uppercase character, 1 lowercase character, and 1 number."/>';
+
+
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+
+  console.log(formData);
+  formData.forEach((value, name) => {
+    console.log('onFormSubmit -> name', name);
+      console.log('onFormSubmit -> value', value);
+      
+  });
+    
+  this.reset();  
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,8 @@
+const changeColorClick = document.querySelector('.change-color');
+changeColorClick.addEventListener('click', getRandomHexColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  document.querySelector('.color').textContent = color;
+  document.querySelector('body').style.backgroundColor = color;
 }
