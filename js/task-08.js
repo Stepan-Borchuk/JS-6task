7fffc045c09d811069fd95dfab4c2fd9038c2822
Stepan <91005125+Stepan-Borchuk@users.noreply.github.com>
2022-04-22 15:14:04 +0300

    8kor1

--- js/task-08.js
+++ js/task-08.js
@@ -1,9 +1,9 @@
 const form = document.querySelector('.login-form');
 
 // Валідація
-form.firstElementChild.innerHTML = '<input type="email" name="email" required="required" />';
+form.firstElementChild.innerHTML = '<input type="email" name="email" required="required" placeholder="Please enter your email"/>';
 
-form.firstElementChild.nextElementSibling.innerHTML = '<input type="password" name="password" required minlength="6" maxlength="12" pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$" title="Please include at least 1 uppercase character, 1 lowercase character, and 1 number."/>';
+form.firstElementChild.nextElementSibling.innerHTML = '<input type="password" name="password" placeholder="Please enter your password include at least 1 uppercase character, 1 lowercase character, and 1 number" required minlength="6" maxlength="12" pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$" title="Please include at least 1 uppercase character, 1 lowercase character, and 1 number."/>';
 
 
 
