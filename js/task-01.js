6465dd989734760c4937ef8f2fd78ef43fb3eb54
Stepan <91005125+Stepan-Borchuk@users.noreply.github.com>
2022-04-24 20:40:43 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,9 @@
 document.querySelectorAll( '.item' )
     .forEach( item => {
         console.log(`Category: ${item.firstElementChild.textContent}`);
-        // console.log(`Elements: ${item.lastElementChild.length}`)
-        console.log(item.lastElementChild)
+        // console.log(`Elements: ${item.querySelectorAll('li')}`)
+        console.log(`Elements: ${item.lastElementChild.children.length}`)
+        // console.log(item.lastElementChild.children.length);
+        // console.log(item.querySelectorAll('li'));
     })
 
